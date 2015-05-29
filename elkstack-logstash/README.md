8d4b5d2e66b8207c75ce9ee100674e5557c4c4e2
Andre Rabold <arabold@nero.com>
2015-05-29 11:17:21 -0700

    Wait for Elasticsearch to be ready before starting Logstash

--- elkstack-logstash/README.md
+++ elkstack-logstash/README.md
@@ -6,3 +6,11 @@ Elasticsearch enough time to startup and run EC2 discovery.
 
 Default config is can be overwritten by mounting a host volume at `/etc/logstash/conf.d/logstash.conf`.
 
+There's a shared data partition that can be used to persist plugin data (e.g. `sincedb` files). Mount
+a host volume at `/usr/share/logstash/data`. Access rights will be set automatically.
+
+Logstash startup will be delayed until there's a running Elasticsearch instance available 
+at http://elasticsearch:9200. So when using this image make sure to not use link your Elasticsearch 
+instance as `elasticsearch` and have it listen on port `9200` (the default). Otherwise this image
+will *not* start up.
+
--- elkstack-logstash/docker-entrypoint.sh
+++ elkstack-logstash/docker-entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Wait for the Elasticsearch container to be ready before starting Logstash.
+# This is especially important if Logstash tries to upload its default template or tries to
+# push events to Elasticsearch while that is still registering itself in the cluster.
+if host elasticsearch >/dev/null; then
+	echo "Stalling for Elasticsearch"
+	while true; do
+		timeout 1 bash -c 'cat < /dev/null > /dev/tcp/elasticsearch/9200' 2>/dev/null && break
+	done
+fi
+
 # Add logstash as command if needed
 if [[ "$1" == -* ]]; then
 	set -- logstash "$@"
@@ -9,6 +19,8 @@ fi
 
 # Run as user "logstash" if the command is "logstash"
 if [ "$1" == logstash ]; then
+	echo "Starting Logstash"
+
 	# Change the ownership of /usr/share/logstash/data to logstash
 	chown -R logstash:logstash /usr/share/logstash/data
 	set -- gosu logstash "$@"
