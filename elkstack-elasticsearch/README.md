bef9c4648f01763072f322db28c49bd30431738e
Andre Rabold <arabold@nero.com>
2015-06-29 07:55:29 -0700

    Increase ulimit to 64000 for ElasticSearch

--- elkstack-elasticsearch/README.md
+++ elkstack-elasticsearch/README.md
@@ -3,5 +3,5 @@ elkstack-elasticsearch
 Based on the official [Elasticsearch](https://registry.hub.docker.com/_/elasticsearch/)
 docker image with additional 
 [Curator](https://www.elastic.co/guide/en/elasticsearch/client/curator/current/index.html), 
-[Elasticsearch Kopf] (https://github.com/lmenezes/elasticsearch-kopf) and 
+[Elasticsearch Kopf](https://github.com/lmenezes/elasticsearch-kopf) and 
 [AWS Cloud Plugin for Elasticsearch](https://github.com/elastic/elasticsearch-cloud-aws).
--- elkstack-elasticsearch/docker-entrypoint.sh
+++ elkstack-elasticsearch/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-#NETWORK_PUBLISH_HOST=${NETWORK_PUBLISH_HOST:-`/sbin/ip route|awk '/default/ { print $3 }'`}
-
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch
@@ -14,8 +12,10 @@ if [ "$1" = 'elasticsearch' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
 	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
 	exec gosu elasticsearch "$@" 
-	#-Des.network.publish_host=$NETWORK_PUBLISH_HOST
 fi
+
+# Make sure ulimit is set appropriately
+ulimit -n 64000
 
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
